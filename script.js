79d6998d4a1af112702841d061c4e624f8ae8f7f
The Code <everett.code93@gmail.com>
2020-03-01 09:12:23 -0600

    broken code still/API working in console-not html text

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ $("#search").on("click", function () {
     console.log("#search");
     //taking value of search 
     var search = $('#text').val();
-    console.log(search);
+   
     //nytimes search engine info
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Austin,Texas&appid=" +
-        search + "dd3678bf60e37d99bb495e887356c617";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        search + "&appid=dd3678bf60e37d99bb495e887356c617";
 
-    // var queryURL =  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=4ooBwyM9KtRfqGUIYHwIamHTT8tMJ8it"
 
 
     $.ajax({
@@ -24,18 +23,19 @@ $("#search").on("click", function () {
         .then(function (response) {
             console.log(response)
             //code to pull search info
-            // var results = response.response.docs;
-            // //loop to push out articles 
-            // console.log(response);
-            // for (var i = 0; i < results.length; i++) {
-            //     var webDiv = $("<div>");
-            //     //pushing urls found on search
-            //     var person = $("<p>").text(results[i].web_url);
-            //     var p = $("<p>").addClass("articles");
-            //     webDiv.append(person);
-            //     p.append(webDiv)
-            //     //showing the results on html
-            //     $("#list").prepend(p);
-            // }console.log(ajax);
+            var results = response;
+            //loop to push out weather info
+            
+            for (var i = 0; i < results.length; i++) {
+                var webDiv = $("<div>");
+                //pushing info found on search
+                var city = $("<p>").text(results[i].city.name);
+                console.log(city);
+                var p = $("<p>").addClass("info");
+                webDiv.append(city);
+                p.append(webDiv)
+                //showing the results on html
+                $("#list").prepend(p);
+            }console.log($.ajax);
         });
 });
